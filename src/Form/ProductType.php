a9dcd6a862ee4863288513d44f80b19e4356383a
rayan <rayanraihani9@gmail.com>
2024-02-13 10:46:42 +0100

    pro

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -18,23 +18,37 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('description', TextareaType::class)
-            ->add('stock', IntegerType::class)
-            ->add('volume', IntegerType::class)
-            ->add('benefits', TextareaType::class)
-            ->add('imgProduct', TextType::class)
-            ->add('nutritionalPropreties', TextareaType::class)
+            ->add('name', TextType::class, [
+                'label' => 'Nom du produit',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+            ])
+            ->add('stock', IntegerType::class, [
+                'label' => 'Quantité en stock',
+            ])
+            ->add('volume', IntegerType::class, [
+                'label' => 'Volume',
+            ])
+            ->add('benefits', TextareaType::class, [
+                'label' => 'Bienfaits',
+            ])
+            ->add('imgProduct', FileType::class, [
+                'label' => 'Image du produit',
+                'mapped' => false, 
+            ])
+            ->add('nutritionalPropreties', TextareaType::class, [
+                'label' => 'Propriétés nutritionnelles',
+            ])
             ->add('category', EntityType::class, [
+                'label' => 'Catégorie',
                 'class' => Category::class,
                 'choice_label' => 'name',
             ])
-            // Assuming Category entity has a 'name' field
-                // Other options can be set here, such as 'multiple' for a many-to-many relationship
-        
-            // Add more fields or customize as needed
-            ;   
+            // ...
+            ;
     }
+    
 
     public function configureOptions(OptionsResolver $resolver): void
     {
